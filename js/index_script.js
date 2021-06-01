2d428bf16382d27eaa4d122a59229d46902a031d
bibhuprasadrout <bibhu.rs@gmail.com>
2021-06-01 11:44:55 +0530

    refactore code - added display pic to blog elements; added button to link to blog

--- js/index_script.js
+++ js/index_script.js
@@ -1,14 +1,8 @@
 function showNavMenu() {
-  if (document.querySelector('.navstop-dropdown').style.width == '110px'){
+  if (document.querySelector('.navstop-dropdown').style.width === '110px') {
     document.querySelector('.navstop-dropdown').style.width = '0';
   }
   else {
-  document.querySelector('.navstop-dropdown').style.width = '110px';
-  }
-}
-
-function articleProperties() {
-  if(document.querySelector('article').style.background === '#3182ce'){
-    document.querySelector('article').style.color = '#fff';
+    document.querySelector('.navstop-dropdown').style.width = '110px';
   }
 }

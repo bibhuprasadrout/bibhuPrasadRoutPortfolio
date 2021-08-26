14b785009cd0a2e3fd9cb558c14d73c7d607732b
bibhuprasadrout <bibhu.rs@gmail.com>
2021-08-26 17:27:33 +0530

    Style added and many new implementations added

--- js/index_script.js
+++ js/index_script.js
@@ -1,8 +1,8 @@
-function showNavMenu() {
+document.querySelector('.navstop').addEventListener('click', function showNavMenu() {
   if (document.querySelector('.navstop-dropdown').style.width === '110px') {
     document.querySelector('.navstop-dropdown').style.width = '0';
   }
   else {
     document.querySelector('.navstop-dropdown').style.width = '110px';
   }
-}
+})
